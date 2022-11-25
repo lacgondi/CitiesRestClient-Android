04dc9937a77cd7b75fc203861e72c56c30e281e5
László Gondi <laszlo.zsolt.gondi@gmail.com>
2022-11-25 18:17:47 +0100

    EI don't know what goes wrong, won't fix

--- app/src/main/java/com/example/varosok/ListActivity.java
+++ app/src/main/java/com/example/varosok/ListActivity.java
@@ -1,42 +1,135 @@
 package com.example.varosok;
 
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.text.method.ScrollingMovementMethod;
+import android.view.View;
+import android.view.accessibility.AccessibilityNodeInfo;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.gson.Gson;
+
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class ListActivity extends AppCompatActivity {
     private Button backButton;
     private TextView list;
+    private List<City> cities = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list);
         init();
-        backButton.setOnClickListener(view -> {
-            Intent intent = new Intent(ListActivity.this, MainActivity.class);
-            startActivity(intent);
-            finish();
+        RequestTask task = new RequestTask(MainActivity.URL, "GET");
+        task.execute();
+
+        backButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent myIntent = new Intent(ListActivity.this, MainActivity.class);
+                ListActivity.this.startActivity(myIntent);
+            }
         });
-        try {
-            list.setText(loadPeopleFromServer());
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
     }
 
-    private void init(){
-        backButton = findViewById(R.id.buttonBack);
+    public void init() {
+        backButton = findViewById(R.id.buttonBackFromInsert);
         list = findViewById(R.id.listOut);
+        list.setMovementMethod(new ScrollingMovementMethod());
     }
 
-    private String loadPeopleFromServer() throws IOException {
-        Response response = RequestHandler.get(MainActivity.URL);
-        return response.getContent();
+    private void formBack() {
+        list.setVisibility(View.GONE);
+        RequestTask task = new RequestTask(MainActivity.URL, "GET");
+        task.execute();
+    }
+
+    private class RequestTask extends AsyncTask<Void, Void, Response> {
+        private String requestUrl;
+        private String requestMethod;
+        private String requestBody;
+
+        public RequestTask(String requestUrl) {
+            this.requestUrl = requestUrl;
+            this.requestMethod = "GET";
+        }
+
+        public RequestTask(String requestUrl, String requestMethod) {
+            this.requestUrl = requestUrl;
+            this.requestMethod = requestMethod;
+        }
+
+        public RequestTask(String requestUrl, String requestMethod, String requestBody) {
+            this.requestUrl = requestUrl;
+            this.requestMethod = requestMethod;
+            this.requestBody = requestBody;
+        }
+
+        @Override
+        protected Response doInBackground(Void... voids) {
+            Response response = null;
+            try {
+                switch (requestMethod) {
+                    case "GET":
+                        response = RequestHandler.get(MainActivity.URL);
+                        break;
+                    case "POST":
+                        response = RequestHandler.post(requestUrl, requestBody);
+                        break;
+                    case "PUT":
+                        response = RequestHandler.put(requestUrl, requestBody);
+                        break;
+                    case "DELETE":
+                        response = RequestHandler.delete(requestUrl);
+                        break;
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return response;
+        }
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+        }
+
+        @Override
+        protected void onPostExecute(Response response) {
+            Gson converter = new Gson();
+            super.onPostExecute(response);
+            if (response == null) {
+                Toast.makeText(ListActivity.this, "unable_to_connect", Toast.LENGTH_SHORT).show();
+                return;
+            }
+            if (response.getResponseCode() >= 400) {
+                Toast.makeText(ListActivity.this, response.getContent(), Toast.LENGTH_SHORT).show();
+                return;
+            }
+            switch (requestMethod) {
+                case "GET":
+                    String content = response.getContent();
+                    cities = Arrays.asList(converter.fromJson(content, City[].class));
+                    System.out.println("!" + cities);
+                    list.setText(cities.toString());
+                    break;
+                default:
+                    if (response.getResponseCode() >= 201 && response.getResponseCode() < 300) {
+                        list.setText("");
+                        RequestTask task = new RequestTask(MainActivity.URL);
+                        task.execute();
+                    }
+                    break;
+            }
+        }
     }
 }
