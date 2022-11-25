7cc0040cf6429131a1fc306bc8fad8c0084c23ba
Gondi László <laszlo.zsolt.gondi@gmail.com>
2022-11-25 08:35:28 +0100

    Add basic layout and functionality

--- app/src/main/java/com/example/varosok/MainActivity.java
+++ app/src/main/java/com/example/varosok/MainActivity.java
@@ -2,15 +2,45 @@ package com.example.varosok;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
     public static final String URL = "https://retoolapi.dev/If7jC8/data";
 
+    private Button add;
+    private Button list;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        init();
+        add.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, AddActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+        list.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, ListActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+    }
+
+    private void init() {
+        add = findViewById(R.id.buttonAdd);
+        list = findViewById(R.id.buttonList);
     }
 }
