0ec66b6db7d62187c74b4b5e21722d0f8b64c9e0
Gondi László <laszlo.zsolt.gondi@gmail.com>
2022-11-25 08:18:26 +0100

    First commit

---
+++ app/src/main/java/com/example/varosok/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.varosok;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    public static final String URL = "https://retoolapi.dev/If7jC8/data";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
