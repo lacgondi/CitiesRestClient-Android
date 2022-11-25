c4848f8d2224cf0f79731d989d8a7cb0e0fb06f3
Gondi László <laszlo.zsolt.gondi@gmail.com>
2022-11-25 08:55:05 +0100

    Add event listener to back button and fetch function for the textview

--- app/src/main/java/com/example/varosok/ListActivity.java
+++ app/src/main/java/com/example/varosok/ListActivity.java
@@ -1,13 +1,51 @@
 package com.example.varosok;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.gson.Gson;
+
+import java.io.IOException;
+import java.util.List;
+
 public class ListActivity extends AppCompatActivity {
+    private Button backButton;
+    private TextView list;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list);
+        init();
+        backButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(ListActivity.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+        try {
+            list.setText(loadPeopleFromServer());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void init(){
+        backButton = findViewById(R.id.buttonBack);
+        list = findViewById(R.id.listOut);
+    }
+
+    private String loadPeopleFromServer() throws IOException {
+        Response response = RequestHandler.get(MainActivity.URL);
+        String content = response.getContent();
+        Gson converter = new Gson();
+        return content;
     }
 }
